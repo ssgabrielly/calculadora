21f231657b55e75c935fe0ec3a5aefbcfe935406
Gabrielly <ssgabi12@gmail.com>
2018-10-25 20:18:38 -0300

    Começando funções dos botões

--- app/src/main/java/com/calc/tecinfo/calculadora/MainActivity.java
+++ app/src/main/java/com/calc/tecinfo/calculadora/MainActivity.java
@@ -2,12 +2,189 @@ package com.calc.tecinfo.calculadora;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private String memoria;
+    private String operacao;
+    private double valor1 = 0;
+    private double valor2 = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final Button btn0 = (Button) findViewById(R.id.btn_0);
+        final Button btn1 = (Button) findViewById(R.id.btn_1);
+        final Button btn2 = (Button) findViewById(R.id.btn_2);
+        final Button btn3 = (Button) findViewById(R.id.btn_3);
+        final Button btn4 = (Button) findViewById(R.id.btn_4);
+        final Button btn5 = (Button) findViewById(R.id.btn_5);
+        final Button btn6 = (Button) findViewById(R.id.btn_6);
+        final Button btn7 = (Button) findViewById(R.id.btn_7);
+        final Button btn8 = (Button) findViewById(R.id.btn_8);
+        final Button btn9 = (Button) findViewById(R.id.btn_9);
+        final Button btnSoma = (Button) findViewById(R.id.btn_soma);
+        final Button btnSub = (Button) findViewById(R.id.btn_subtracao);
+        final Button btnMult = (Button) findViewById(R.id.btn_multiplicacao);
+        final Button btnDiv = (Button) findViewById(R.id.btn_divisao);
+        final Button btnC = (Button) findViewById(R.id.btn_apagar);
+        final Button btnVirg = (Button) findViewById(R.id.btn_virgula);
+        final Button btnIgual = (Button) findViewById(R.id.btn_igual);
+
+        final TextView dConta = (TextView) findViewById(R.id.displayConta);
+        final TextView dResult = (TextView) findViewById(R.id.displayResultado);
+
+        btn0.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                memoria = (String) dConta.getText();
+                dConta.setText(memoria + "0");
+            }
+        });
+
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                memoria = (String) dConta.getText();
+                dConta.setText(memoria + "1");
+            }
+        });
+
+        btn2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                memoria = (String) dConta.getText();
+                dConta.setText(memoria + "2");
+            }
+        });
+
+        btn3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                memoria = (String) dConta.getText();
+                dConta.setText(memoria + "3");
+            }
+        });
+
+        btn4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                memoria = (String) dConta.getText();
+                dConta.setText(memoria + "4");
+            }
+        });
+
+        btn5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                memoria = (String) dConta.getText();
+                dConta.setText(memoria + "5");
+            }
+        });
+
+        btn6.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                memoria = (String) dConta.getText();
+                dConta.setText(memoria + "6");
+            }
+        });
+
+        btn7.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                memoria = (String) dConta.getText();
+                dConta.setText(memoria + "7");
+            }
+        });
+
+        btn8.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                memoria = (String) dConta.getText();
+                dConta.setText(memoria + "8");
+            }
+        });
+
+        btn9.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                memoria = (String) dConta.getText();
+                dConta.setText(memoria + "9");
+            }
+        });
+
+        btnVirg.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                memoria = (String) dConta.getText();
+                dConta.setText(memoria + ",");
+            }
+        });
+
+        btnC.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                memoria = "";
+                dConta.setText("");
+                dResult.setText("");
+            }
+        });
+
+        btnSoma.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                operacao = "+";
+                valor1 = Double.parseDouble(dConta.getText().toString());
+                dResult.setText(dConta.getText().toString() + "+");
+                dConta.setText("");
+            }
+        });
+
+        btnSub.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                operacao = "-";
+                valor1 = Double.parseDouble(dConta.getText().toString());
+                dResult.setText(dConta.getText().toString() + "-");
+                dConta.setText("");
+            }
+        });
+
+        btnMult.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                operacao = "*";
+                valor1 = Double.parseDouble(dConta.getText().toString());
+                dResult.setText(dConta.getText().toString() + "*");
+                dConta.setText("");
+            }
+        });
+
+        btnDiv.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                operacao = "/";
+                valor1 = Double.parseDouble(dConta.getText().toString());
+                dResult.setText(dConta.getText().toString() + "/");
+                dConta.setText("");
+            }
+        });
+
+        btnIgual.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (dConta.getText().equals("")) {
+                    Toast.makeText(getApplicationContext(), "Digite um número!", Toast.LENGTH_LONG).show());
+                }
+            }
+        });
+
     }
 }
